cb201fcf324d5e0e0a67d311aa751a463a9c0486
unknown <CETECOM@AOAWSB02LC0619.duoc.acad>
2025-05-19 14:42:00 -0400

    configuracion dependencias de cursos y permisos services + correcion de comentarios repository

--- code/cursos-services/courses/src/main/java/com/cursos/service/courses/repositorio/CursoRepository.java
+++ code/cursos-services/courses/src/main/java/com/cursos/service/courses/repositorio/CursoRepository.java
@@ -6,11 +6,5 @@ import com.cursos.service.courses.entidades.Curso;
 
 @Repository
 public interface CursoRepository extends JpaRepository<Curso, Integer> {
-    /*
-    * findAll() --> Retorna todos los cursos
-    * findById(Integer id) --> Retorna un curso según su id
-    * save(Curso curso) --> Guarda o agrega un nuevo curso.
-    * Si el id ya existe, actualiza el curso
-    * deleteById(Integer id) --> Elimina un curso según su id
-    */
+
 }
