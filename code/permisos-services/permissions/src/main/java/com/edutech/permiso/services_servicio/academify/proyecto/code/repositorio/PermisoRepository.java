d7993f784803b96ad03c4f079a18920f96843bb8
unknown <CETECOM@AOAWSB02LC0619.duoc.acad>
2025-05-19 14:39:25 -0400

    modificacion en comentarios de los repository

--- code/permisos-services/permissions/src/main/java/com/edutech/permiso/services_servicio/academify/proyecto/code/repositorio/PermisoRepository.java
+++ code/permisos-services/permissions/src/main/java/com/edutech/permiso/services_servicio/academify/proyecto/code/repositorio/PermisoRepository.java
@@ -7,8 +7,5 @@ import com.edutech.permiso.services_servicio.academify.proyecto.code.entidad.Per
 
 @Repository
 public interface PermisoRepository extends JpaRepository<Permiso, Integer> {
-    // findAll() -- Retorna todos los permisos
-    // findById(Integer id) -- Retorna un permiso según su id
-    // save(Permiso permiso) -- Guarda o agrega un nuevo permiso. Si el id ya existe, actualiza el permiso
-    // deleteById(Integer id) -- Elimina un permiso según su id
+
 }
